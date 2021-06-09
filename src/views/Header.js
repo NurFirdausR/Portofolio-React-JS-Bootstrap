9dd0d0d7d449d34ce28ff542b503e6a8b524ffc6
NurFirdausR <nurfirdaus.5000@gmail.com>
2021-06-09 20:14:25 +0700

    40%

---
+++ src/views/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = () => {
+    return (
+        <header class="masthead">
+        <div class="container">
+          <div class="masthead-subheading">Nur Firdaus!</div>
+          <div class="masthead-heading text-uppercase">Learn React-JS</div>
+          <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+        </div>
+      </header>
+    )
+}
+
+// Header.propTypes = {
+
+// }
+
+export default Header
